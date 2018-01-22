281ea3975938d347e7080c57e42b17fc10b34b12
Ephigenia <love@ephigenia.de>
2018-01-22 12:19:59 +0100

    feat: adds finishedText & finishedTheme options

--- public/main.js
+++ public/main.js
@@ -2,6 +2,8 @@ var config = {
   title: '',
   theme: 'monokai',
   to: new Date(2030, 11, 24),
+  finishedText: '00:00:00',
+  finishedTheme: 'summer',
   dateFormat: 'LLLL',
   updateInterval: 1000
 }
@@ -10,11 +12,18 @@ var config = {
 var params = new URLSearchParams(window.location.search);
 config.title = params.get('title') || config.title;
 config.theme = params.get('theme') || config.theme;
+config.finishedTheme = params.get('finishedTheme') || config.finishedTheme;
+config.finishedText = params.get('finishedText') || config.finishedText;
 if (params.get('to')) {
   config.to = moment(params.get('to')).toDate();
 }
 
-document.body.className = 'theme-' + config.theme;
+// set theme
+function setTheme(name) {
+  document.body.className = 'theme-' + name;
+}
+
+setTheme(config.theme);
 
 // update title & target date display
 document.getElementById('title').innerHTML = config.title;
@@ -25,18 +34,26 @@ setInterval(() => {
   var diff = config.to - now;
   var days = moment(config.to).diff(moment(), 'days');
   var countdown = '';
+  
+  // with days?
   if (days > 0) {
     if (days > 999) {
       days = 999;
     }
     countdown = days + 'd ';
-  }
-  if (diff > 0) {
     document.getElementById('countdown').classList.className = '';
-    countdown += moment(diff).utc().format('HH:mm:ss');
   } else {
     document.getElementById('countdown').classList.className = 'no-days';
-    countdown = '00:00:00'
   }
+
+  // countdown target date & time reached?
+  if (diff > 0) {
+    countdown += moment(diff).utc().format('HH:mm:ss');
+  } else {
+    setTheme(config.finishedTheme);
+    countdown = config.finishedText;
+  }
+
+  // finally set the countdown value
   document.getElementById('countdown').innerHTML = countdown;
 }, config.updateInterval);
